61a9ea06799c4cb953708e64cac94b5b22a96c1c
Pavel Patalashko <foxthesaw@gmail.com>
2022-04-21 00:30:43 +0300

    implemented filters, added SG function

--- deploy.py
+++ deploy.py
@@ -7,8 +7,8 @@ import sys
 # root.mainloop()
 
 AWS_REGION = "eu-central-1"
-# VPC_ID = "vpc-05a63425b4ac937e4" # eu-central
-VPC_ID = "vpc-81cf74f8"  # eu-west-1
+VPC_ID = "vpc-05a63425b4ac937e4" # eu-central
+# VPC_ID = "vpc-81cf74f8"  # eu-west-1
 
 Config = botocore.config.Config(region_name=AWS_REGION)
 
@@ -25,27 +25,51 @@ client = boto3.client(
 )
 
 ec2 = boto3.resource('ec2', region_name=AWS_REGION)
-vpc = ec2.Vpc(id=VPC_ID)
 
 def get_public_subnets():
     public_subnets = []
-    apicall = client.describe_route_tables()
+    apicall = client.describe_route_tables(
+        Filters=[
+            {   
+                'Name': 'vpc-id',
+                'Values': [
+                    VPC_ID,
+                ],
+                'Name': 'route.state',
+                'Values': [
+                    'active',
+                ],
+                'Name': 'route.destination-cidr-block',
+                'Values': [
+                    '0.0.0.0/0',
+                ]
+            },
+        ]
+    )
     for routeTable in apicall['RouteTables']:
         associations = routeTable['Associations']
-        routes = routeTable['Routes']
-        isPublic = False
-        for route in routes:
-            gateway_id = route.get('GatewayId')
-            if gateway_id.startswith('igw-'):
-                isPublic = True
-        if(not isPublic):
-            continue
         for assoc in associations:
             # This checks for explicit associations, only
             subnetId = assoc.get('SubnetId')
             if subnetId:
                 public_subnets.append(subnetId)
-    if public_subnets:
-        return public_subnets
+    return public_subnets if public_subnets else print(f"No public subnets in {VPC_ID}")       
 
-print(get_public_subnets())
+def configure_security_groups():
+    apicall = client.describe_security_groups(
+        Filters=[
+            {
+                'Name': 'vpc-id',
+                'Values': [
+                        VPC_ID,
+                ]
+            },
+        ]
+    )
+    print(apicall)
+
+def create_ec2_instance():
+    pass
+
+# print(get_public_subnets())
+configure_security_groups()
