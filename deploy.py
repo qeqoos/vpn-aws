36940db25a4bd0b9a9ae2751f41e6c615e1df9ab
Pavel Patalashko <foxthesaw@gmail.com>
2022-04-25 18:11:15 +0300

    added create_peer function. added role and profile function

--- deploy.py
+++ deploy.py
@@ -1,9 +1,9 @@
 # import tkinter as tk
-import re
 import boto3
 import botocore
-import sys
 import time
+from datetime import datetime
+import json
 
 # root = tk.Tk()
 # root.mainloop()
@@ -13,7 +13,9 @@ AWS_REGION = 'eu-west-1'
 VPC_ID = 'vpc-81cf74f8'  # eu-west-1
 PROTOCOL_NAME = 'Wireguard'
 WG_PORT = '51820' # create check for 49152-65530
+SUBNET_CHOICE = 0
 CREATION_TIMEOUT_MINS = 4 
+PEER_NAME = 'pablo'
 
 Config = botocore.config.Config(region_name=AWS_REGION)
 
@@ -30,6 +32,18 @@ client = boto3.client(
 )
 
 ec2 = boto3.resource('ec2', region_name=AWS_REGION)
+
+# vpc_cidr_block = client.describe_vpcs(VpcIds=[VPC_ID])['Vpcs'][0]['CidrBlock']
+# used_octets = vpc_cidr_block.split('.')[0] + '.' + vpc_cidr_block.split('.')[1]
+# peer_private_ips = used_octets + '.69.0/24'
+
+# with open('wg_create_peer.sh', 'r') as peer_script:
+#     wg_create_peer = peer_script.read()
+#     wg_create_peer = wg_create_peer.replace('*peer_name*', PEER_NAME)
+#     wg_create_peer = wg_create_peer.replace('*port*', WG_PORT)
+#     wg_create_peer = wg_create_peer.replace(
+#         '*peer_private_ips*', peer_private_ips)
+
 
 def get_public_subnets():
     public_subnets = []
@@ -177,18 +191,72 @@ def get_instance_id(SUBNET_CHOICE, subnet_list):
     return instance_id
 
 
+def get_profile_name_arn():
+    iam_client = boto3.client('iam')
+    try:
+        role_check_apicall = iam_client.get_role(RoleName='EC2SSMrole')
+    except Exception as e:
+        print(e)
+        print('Creating role...')
+        assume_role_policy_document = json.dumps({
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Principal": {
+                        "Service": "ec2.amazonaws.com"
+                    },
+                    "Action": "sts:AssumeRole"
+                }
+            ]
+        })
+        create_role_apicall = iam_client.create_role(
+            RoleName='EC2SSMrole',
+            AssumeRolePolicyDocument=assume_role_policy_document,
+            Description='For script execution my SSM.'
+        )
+        attach_policy_apicall = iam_client.attach_role_policy(
+            RoleName='EC2SSMrole',
+            PolicyArn='arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore'
+        )
+        
+    try:
+        profile_check_apicall = iam_client.get_instance_profile(InstanceProfileName='EC2SSMprofile')
+    except Exception as e:
+        print(e)
+        print('Creating profile and assuming role...')
+        role_check_apicall = iam_client.get_role(RoleName='EC2SSMrole')
+        create_profile_apicall = iam_client.create_instance_profile(InstanceProfileName='EC2SSMprofile')
+        add_role_profile_apicall = iam_client.add_role_to_instance_profile(InstanceProfileName='EC2SSMprofile', RoleName='EC2SSMrole')
+        profile_check_apicall = iam_client.get_instance_profile(InstanceProfileName='EC2SSMprofile')
+
+    return profile_check_apicall
+    
+
 def create_ec2_instance_main():
-    SUBNET_CHOICE = 0
     subnet_list = get_public_subnets()
     instance_id = get_instance_id(SUBNET_CHOICE, subnet_list)
 
     if instance_id:
         print(f'{PROTOCOL_NAME} VPN server already exists in subnet {subnet_list[SUBNET_CHOICE]}.')
-        print(f'InstanceId is {instance_id}')
+        print(f'InstanceId is {instance_id[0]["Instances"][0]["InstanceId"]}, public IP {instance_id[0]["Instances"][0]["PublicIpAddress"]}')
     else:
         print(f'No {PROTOCOL_NAME} VPN server found in subnet {subnet_list[SUBNET_CHOICE]}, creating one...') 
+        
         image_id = get_ami()
         security_group_id = configure_security_groups()
+        ec2_profile = get_profile_name_arn()
+
+        key_name = 'vpn_ssh_' + str(datetime.now().strftime('%Y%m%d%H%M%S'))
+        keypair_apicall = client.create_key_pair(KeyName=key_name, KeyType='rsa')
+        with open(key_name, 'w') as file:
+            file.write(keypair_apicall['KeyMaterial'])
+        print('Change flie permissions to 600.')
+
+        with open('wg_install.sh', 'r') as install_script:
+            wg_install = install_script.read()
+            wg_install = wg_install.replace('*port*', WG_PORT)
+
         print('Prerequisites finshed. Started creation of server...')
         try:     
             server = ec2.create_instances(
@@ -206,10 +274,13 @@ def create_ec2_instance_main():
                 ],
                 ImageId=image_id,
                 InstanceType='t2.micro',
-                # KeyName='string',
+                KeyName=key_name,
                 # DryRun=True,
                 MaxCount=1,
                 MinCount=1,
+                IamInstanceProfile={
+                    'Arn': ec2_profile['InstanceProfile']['Arn'],
+                },
                 Monitoring={
                     'Enabled': False
                 },
@@ -221,12 +292,12 @@ def create_ec2_instance_main():
                         'Groups': [
                             security_group_id,
                         ],
-                        # 'PrivateIpAddress': 'string',
+                        # 'PrivateIpAddress': vpn_instance_private_ip,
                         'SubnetId': subnet_list[SUBNET_CHOICE],
                         'InterfaceType': 'interface',
                     },
                 ],
-                # UserData='string',
+                UserData=wg_install,
                 InstanceInitiatedShutdownBehavior='stop', 
                 TagSpecifications=[
                     {
@@ -251,13 +322,11 @@ def create_ec2_instance_main():
         except Exception as e:
             print(f'Error in server configuration - {e}')
         
-        instance_id = get_instance_id(SUBNET_CHOICE, subnet_list)[0]['Instances'][0]['InstanceId']
+        instance_id = get_instance_id(SUBNET_CHOICE, subnet_list)[0]['Instances'][0]
         timeout_check = 0 # 4 minutes creation timeout
         while True:
             get_status_apicall = client.describe_instance_status(
-                InstanceIds=[
-                    instance_id,
-                ],
+                InstanceIds=[instance_id['InstanceId']],
                 Filters=[
                     {
                         'Name': 'instance-state-name',
@@ -272,10 +341,10 @@ def create_ec2_instance_main():
                         'Values': ['ok']
                     },
                 ],
-                # DryRun=True,
             )
             if get_status_apicall['InstanceStatuses']:
                 print('Instance is ready.')
+                print(f'Public IP {instance_id["PublicIpAddress"]}.')
                 break
             elif timeout_check == CREATION_TIMEOUT_MINS * 6:
                 print(f'Instance creation exceeded {CREATION_TIMEOUT_MINS} minutes. Stopping...')
@@ -287,4 +356,37 @@ def create_ec2_instance_main():
 
 
 # create_ec2_instance_main()
-# 
+
+
+def create_peer():
+    ssm_client = boto3.client('ssm', config=Config)  # Need your credentials here
+    subnet_list = get_public_subnets()
+    instance_id = get_instance_id(SUBNET_CHOICE, subnet_list)[0]['Instances'][0]['InstanceId']
+    
+    if instance_id:
+        vpc_cidr_block = client.describe_vpcs(VpcIds=[VPC_ID])['Vpcs'][0]['CidrBlock']
+        used_octets = vpc_cidr_block.split('.')[0] + '.' + vpc_cidr_block.split('.')[1]
+        peer_private_ips = used_octets + '.69.0/24'
+        with open('wg_create_peer.sh', 'r') as peer_script:
+            wg_create_peer = peer_script.read()
+            wg_create_peer = wg_create_peer.replace('*peer_name*', PEER_NAME)
+            wg_create_peer = wg_create_peer.replace('*port*', WG_PORT)
+            wg_create_peer = wg_create_peer.replace('*peer_private_ips*', peer_private_ips)
+
+        exec_script = ssm_client.send_command(
+            DocumentName="AWS-RunShellScript",  # One of AWS' preconfigured documents
+            Parameters={'commands': [wg_create_peer]},
+            InstanceIds=[instance_id]
+        )
+
+        time.sleep(5)  # Avoid immeditate retrival
+        command_id = exec_script['Command']['CommandId']
+        output = ssm_client.get_command_invocation(
+            CommandId=command_id,
+            InstanceId=instance_id
+        )
+        return output['StandardOutputContent']
+    else:
+        print('No WG instances found. Please, create one.')
+
+print(create_peer())
