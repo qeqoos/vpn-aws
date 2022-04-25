c661b2081c08035e4259a6a3ac05a6a0d52b36a2
Pavel Patalashko <foxthesaw@gmail.com>
2022-04-25 18:10:28 +0300

    added WG scripts

---
+++ wg_create_peer.sh
@@ -0,0 +1,26 @@
+mkdir /etc/wireguard/*peer_name*
+wg genkey | tee /etc/wireguard/*peer_name*/*peer_name*_privatekey | wg pubkey > /etc/wireguard/*peer_name*/*peer_name*_publickey
+
+cat <<EOT >> /etc/wireguard/wg0.conf
+
+[Peer]
+PublicKey = $(cat /etc/wireguard/*peer_name*/*peer_name*_publickey)
+AllowedIPs = *peer_private_ips*
+EOT
+
+systemctl restart wg-quick@wg0
+
+cat <<EOT > /etc/wireguard/*peer_name*/*peer_name*_client_file
+[Interface]
+PrivateKey = $(cat /etc/wireguard/*peer_name*/*peer_name*_privatekey)
+Address = *peer_private_ips*
+DNS = 8.8.8.8
+
+[Peer]
+PublicKey = $(cat /etc/wireguard/publickey)
+Endpoint = $(curl http://169.254.169.254/latest/meta-data/public-ipv4):*port*
+AllowedIPs = 0.0.0.0/0
+PersistentKeepalive = 20
+EOT
+
+cat /etc/wireguard/*peer_name*/*peer_name*_client_file
---
+++ wg_install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+apt update && apt upgrade -y
+apt install -y wireguard
+
+wg genkey | tee /etc/wireguard/privatekey | wg pubkey | tee /etc/wireguard/publickey
+chmod 600 /etc/wireguard/privatekey
+touch /etc/wireguard/wg0.conf
+
+cat <<EOT > /etc/wireguard/wg0.conf
+[Interface]
+PrivateKey = $(cat /etc/wireguard/privatekey)
+Address = $(curl http://169.254.169.254/latest/meta-data/local-ipv4)/24
+ListenPort = *port*
+PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
+EOT
+
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+sysctl -p
+systemctl enable wg-quick@wg0.service
+systemctl start wg-quick@wg0.service
